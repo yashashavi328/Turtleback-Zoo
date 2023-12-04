2a5a607252e455ca279bad6d4be982c09a7cbe6a
yashashavi328 <yashashavi328@gmail.com>
2023-12-03 23:38:37 -0500

    added basic tkinter window

---
+++ Interface.py
@@ -0,0 +1,14 @@
+# Turtleback Zoo GUI Application
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Turtleback Zoo")
+window.geometry("800x600")
+
+# Code to add widgets will go here...
+
+
+# Code to add widgets ends here...
+
+window.mainloop()
